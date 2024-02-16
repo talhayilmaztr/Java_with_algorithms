f985acd0cb84dae67e7eb1ebf47a7a7f070fdaeb
talhayilmaztr <yilmaz.talha.tr@gmail.com>
2024-02-17 01:37:14 +0300

    Added comments for better readability

--- Student_grade.java
+++ Student_grade.java
@@ -1,3 +1,27 @@
+/* 
+This Java program, named Student_grade, allows users to input an answer key
+and assess the grades of three students based on their answers.
+The program iterates through each student, comparing their answers 
+with the provided answer key. The grades are calculated by awarding 
+20 points for each correct answer. The program also identifies the 
+student with the highest grade and displays their ID.
+
+To use the program, the user needs to input a 5-character answer key 
+and the individual answers of each student. The program then outputs 
+the grades for each student, the maximum grade achieved, and the 
+corresponding student ID.
+
+Example Usage:
+1. Enter the answer key (5 characters): ABCDE
+2. Enter the answers for Student 1 (5 characters): ABCDE
+   Student 1 Grade: 100
+3. Enter the answers for Student 2 (5 characters): BCDEA
+   Student 2 Grade: 0
+4. Enter the answers for Student 3 (5 characters): ABCDE
+   Student 3 Grade: 100
+Max Grade: 100
+ID with the highest grade: 1
+*/
 import java.util.Scanner;
 public class Student_grade { 
     public static void main(String[] args) {
