c5017d279437fb162d609513ee1b5e9b1fdaaea9
talhayilmaztr <yilmaz.talha.tr@gmail.com>
2024-05-12 15:08:08 +0300

    Added password validation functionality

--- Password.java
+++ Password.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Password {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter Your Password");
+        System.out.println("Enter Your Password: ");
         String your_password = input.nextLine();
 
         String result = password(your_password);
@@ -15,9 +15,9 @@ public class Password {
         int digitCount = 0;
         int upper = 0;
 
-        if (password.length() < 6)
+        if (password.length() < 6){
             return "Invalid password: Password length should be at least 6 characters.";
-
+        }
         for (int i = 0; i < password.length(); i++) {
             char currentChar = password.charAt(i);
             
@@ -29,14 +29,16 @@ public class Password {
                 return "Invalid password: Password should contain only digits, uppercase letters, and special characters '!&%'.";
             }
         }
-        if (digitCount < 2)
+        if (digitCount < 2){
             return "Invalid password: Password should contain at least 2 digits.";
-
-        if (upper < 1)
+        }
+        if (upper < 1){
             return "Invalid password: Password should contain at least 1 uppercase letter.";
+        }
         return "Valid password";
     }
-}
+
+    }
 
     
 
